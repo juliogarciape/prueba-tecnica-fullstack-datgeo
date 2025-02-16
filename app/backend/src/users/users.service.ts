9656d0e59f61d9c2ac58be5ac5aa52021728c88a
Julio Cesar <juliogarciamelgarejo@gmail.com>
2025-02-16 01:36:10 -0500

    feat(backend): enhance user creation with detailed DTO and service integration

--- app/backend/src/users/users.service.ts
+++ app/backend/src/users/users.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+
+  createUser(user: CreateUserDto) {
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
   }
 
   findAll() {
